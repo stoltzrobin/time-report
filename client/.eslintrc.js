f91b79db6ea685c8e8c9e99235a22b0c1e088d45
robin <robin.stoltz@netlight.com>
2018-02-22 08:00:22 +0100

    Updated eslint

---
+++ client/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+      ],
+    "plugins": [
+        "react"
+    ],
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaFeatures": {
+          "jsx": true
+        }
+      },
+};
